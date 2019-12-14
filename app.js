7969b29ef490ab0ac68ce6abb924d5a9c575fedf
Juan Vidal <juanvidaldom@gmail.com>
2019-12-14 09:49:35 -0600

    app.js file setted, config folder with configurations for multer and passport created and done, User model file created, but empty

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-
+const express = require('express');
+const favicon = require('serve-favicon');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const passport = require('./config/passport')
+const multer = require('./config/multer')
+const flash = require('connect-flash')
+const cors = require('cors')
 
 mongoose
-  .connect('mongodb://localhost/backend', {useNewUrlParser: true})
+  .connect(process.env.DB, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -24,11 +28,36 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 
 const app = express();
 
+app.use(
+  cors({
+    credentials: true,
+    origin: [process.env.ORIGIN]
+  })
+)
+
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(
+  session({
+    secret: 'SECRET',
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 1000 * 60 * 60 * 24
+    })
+  })
+)
+
+app.use(flash())
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Express View engine setup
 
@@ -36,23 +65,17 @@ app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
-}));
-      
+}));   
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
-
+app.locals.title = 'Positive(+) aid';
 
 const index = require('./routes/index');
-app.use('/', index);
-
+app.use('/api', index);
+app.get('/*', (req, res) => res.sendFile(__dirname + '/public/index.html'))
 
 module.exports = app;
